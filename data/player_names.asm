2c0b7efe85fedd70a109e2979fb0a207c3d0c31a
KingMayro <52179117+KingMayro@users.noreply.github.com>
2023-06-10 16:41:05 -0400

    source

---
+++ data/player_names.asm
@@ -0,0 +1,13 @@
+DefaultNamesPlayer:
+	db   "NEW NAME"
+	next "PURPLE"
+	next "ASH"
+	next "GRAYSON"
+	db   "@"
+
+DefaultNamesRival:
+	db   "NEW NAME"
+	next "BLUE"
+	next "GARY"
+	next "JEFF"
+	db   "@"
---
+++ data/player_names_list.asm
@@ -0,0 +1,11 @@
+DefaultNamesPlayerList:
+	db "NEW NAME@"
+	db "PURPLE@"
+	db "ASH@"
+	db "GRAYSON@"
+
+DefaultNamesRivalList:
+	db "NEW NAME@"
+	db "BLUE@"
+	db "GARY@"
+	db "JEFF@"
